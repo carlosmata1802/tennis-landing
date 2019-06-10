d7645e2e52c7ef03f8f0e04a9f55768d182f1d57
carlosmata1802 <charlixd.cm@gmail.com>
2019-06-09 19:12:48 -0500

    Responsive complete

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,9 +1,32 @@
-const toggle_menu = action => (document.getElementsByClassName("mobile_menu_items_content")[0].style.right = (action) ?  "0" : "-80%")
-
+const toggle_menu = action => (document.getElementsByClassName("mobile_menu_items_content")[0].style.right = (action) ?  "0" : "-80%");
 const scroll_nav = () => {
   let nav = document.getElementsByClassName("header_content")[0];
-  window.onscroll = () => {
-    (window.pageYOffset > 80) ? nav.classList.add("shadow") : nav.classList.remove("shadow");
-  }
+  window.onscroll = () => ((window.pageYOffset > 80) ? nav.classList.add("shadow") : nav.classList.remove("shadow"));
 }
-scroll_nav(); 
+scroll_nav();
+
+let count = 0
+const slider = action => {
+let dots = document.getElementsByClassName("slide_dots")[0];
+let items_content = document.getElementsByClassName("items");
+let val = -100
+if (count < 4) count = (action) ? count + 1 : count - 1;
+
+if (count < 4 && count > -1) var move = count*val
+if (count == 4 ) {
+  move = 0;
+  count = 0;
+}else if(count < 0 && action == false){
+  move = -300;
+  count = 3;
+}
+
+for (var i = 0; i < items_content.length; i++) {
+  console.log(items_content[i]);
+  items_content[i].style.left = `${move}%`;
+}
+for (var i = 0; i < dots.children.length; i++) {
+  dots.children[i].classList.remove("active");
+}
+dots.children[count].classList.add("active");
+}
